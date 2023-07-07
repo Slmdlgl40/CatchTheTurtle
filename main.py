f598126a0b2ca27239dedd140064e01f9ea948ed
Salim Dalgalı <sdalgali03@gmail.com>
2023-07-07 11:39:36 -0500

    kodlar daha da düzenlenip score tabelası oluşturuldu

--- main.py
+++ main.py
@@ -7,31 +7,41 @@ turtle_screen.bgcolor("light blue")
 turtle_screen.title("Catch The Turtle")
 kaplumbaga = turtle.Turtle()
 sayac = turtle.Turtle()
+score = turtle.Turtle()
 def catch_turtle(saniye):
+    def rastgele_konum():
+        x = random.randint(-400, 400)
+        y = random.randint(-250, 220)
+        kaplumbaga.goto(x, y)
+
     yukseklik = turtle.window_height() // 2 - 100
+    puan = 0
     sayac.up()
     kaplumbaga.up()
+    score.up()
+    score.hideturtle()
     sayac.hideturtle()
     kaplumbaga.shape("turtle")
     kaplumbaga.shapesize(2,2,1)
+
     while saniye >= 0:
         sayac.clear()
         sayac.sety(yukseklik)
         sayac.write(str(saniye),align="center",font=("Arial", 70 ,"normal"))
-        time.sleep(1)
-        saniye -= 1
+        score.clear()
+        score.sety(yukseklik - 70)
+        score.write("Score:" + str(puan),align="center",font=("Arial", 50 ,"normal"))
         kaplumbaga.hideturtle()
         rastgele_konum()
         kaplumbaga.showturtle()
+        time.sleep(1)
+        saniye -= 1
 
     sayac.clear()
-    sayac.sety(yukseklik)
     sayac.write("Süre Bitti!", align='center', font=('Arial', 70, 'normal'))
+    score.clear()
+    score.write("Score:" + str(puan),align="center",font=("Arial", 50 ,"normal"))
 
-def rastgele_konum():
-    x = random.randint(-400,400)
-    y = random.randint(-250,220)
-    kaplumbaga.goto(x,y)
 
 catch_turtle(5)
 turtle.mainloop()
